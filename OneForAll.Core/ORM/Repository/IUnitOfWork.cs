19f256ae52a3b11df63a26614631cdc60a3ab151
xhp <xhp@XHP>
2022-11-13 00:57:48 +0800

    调整UnitOfWork的BeginTransaction

--- OneForAll.Core/ORM/Repository/IUnitOfWork.cs
+++ OneForAll.Core/ORM/Repository/IUnitOfWork.cs
@@ -3,15 +3,32 @@ using System.Collections.Generic;
 
 namespace OneForAll.Core.ORM
 {
-
+    /// <summary>
+    /// 工作单元
+    /// </summary>
     public interface IUnitOfWork : IDisposable
     {
+        /// <summary>
+        /// 异常信息
+        /// </summary>
         HashSet<Exception> Exceptions { get; set; }
 
-        IUnitTransaction BeginTransaction();
+        /// <summary>
+        /// 注册事务
+        /// </summary>
+        /// <param name="transactionType">事务类型</param>
+        /// <returns></returns>
+        IUnitTransaction BeginTransaction(TransactionType transactionType);
 
+        /// <summary>
+        /// 提交所有事务并返回影响行数
+        /// </summary>
+        /// <returns></returns>
         int Commit();
 
+        /// <summary>
+        /// 回滚所有事务
+        /// </summary>
         void RollBack();
 
     }
--- OneForAll.Core/ORM/Repository/IUnitTransaction.cs
+++ OneForAll.Core/ORM/Repository/IUnitTransaction.cs
@@ -4,12 +4,33 @@ using System.Data;
 
 namespace OneForAll.Core.ORM
 {
+    /// <summary>
+    /// 单元事务
+    /// </summary>
     public interface IUnitTransaction : IDisposable
     {
+        /// <summary>
+        /// 事务是否已提交
+        /// </summary>
         bool Commited { get; set; }
-        void Register<T>(Func<int> action, T conn);
-        int Commit(TransactionType tranType = TransactionType.Local);
 
+        /// <summary>
+        /// 注册方法
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="action">方法</param>
+        /// <param name="conn">连接上下文</param>
+        void Register<T>(Func<int> action, T conn);
+
+        /// <summary>
+        /// 提交事务
+        /// </summary>
+        /// <returns>受影响行数</returns>
+        int Commit();
+
+        /// <summary>
+        /// 回滚
+        /// </summary>
         void RollBack();
     }
 }
